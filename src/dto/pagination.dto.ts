fd69405cfc1dbaac47d3b709057be59a541ec96e
molero3111 <molero3111@gmail.com>
2025-01-26 23:08:34 -0300

    Removed max import

--- src/dto/pagination.dto.ts
+++ src/dto/pagination.dto.ts
@@ -1,4 +1,4 @@
-import { IsInt, IsOptional, IsPositive, Max, Min } from 'class-validator';
+import { IsInt, IsOptional, IsPositive, Min } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class PaginationDto {
